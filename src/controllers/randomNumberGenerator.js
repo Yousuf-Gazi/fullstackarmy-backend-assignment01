e68bb88f641d1ed418bca71ca0eb050b46e7e6f3
MD YOUSUF GAZI <gaziyousufridoy@gmail.com>
2023-05-12 18:26:08 +0600

    Feat: fix the bug in randomNumberGenerator

--- src/controllers/randomNumberGenerator.js
+++ src/controllers/randomNumberGenerator.js
@@ -1,6 +1,13 @@
 const generateRandomValue = (req, res) => {
     const { start, end } = req.query;
-    const randomValue = Math.floor(Math.random() * (end - start + 1) + start);
+    const startNum = parseInt(start);
+    const endNum = parseInt(end);
+
+    if (isNaN(startNum) || isNaN(endNum)) {
+        return res.status(400).json({ error: 'Invalid start or end value' });
+    }
+
+    const randomValue = Math.floor(Math.random() * (endNum - startNum + 1) + startNum);
     res.json({ randomValue });
 };
 
